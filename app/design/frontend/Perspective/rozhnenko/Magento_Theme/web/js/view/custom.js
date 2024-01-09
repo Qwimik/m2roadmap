c9bc4c9358c2788043a6aa34ad964b3d4a7113b1
qwimik <qwimik@gmail.com>
2024-01-09 15:02:56 +0200

    ui components in magento 2 observable

--- app/design/frontend/Perspective/rozhnenko/Magento_Theme/web/js/view/custom.js
+++ app/design/frontend/Perspective/rozhnenko/Magento_Theme/web/js/view/custom.js
@@ -2,13 +2,36 @@ define(['uiComponent', 'ko'], (uiComponent, ko) => {
     'use strict';
 
     return uiComponent.extend({
+        // defaults: {
+        //     template: 'Magento_Theme/custom',
+        //     isActive: ko.observable(true),
+        //     text: ko.observable('default text'),
+        //     items: ko.observableArray(['item', 'item'])
+        // }, //
         defaults: {
             template: 'Magento_Theme/custom',
-            isActive: ko.observable(false)
+            isActive: ko.observable(true),
+            text: ko.observable('default text'),
+            items: ko.observableArray(['item', 'item'])
+        },
+
+        initObservable() {
+            this._super();
+
+            // this.text = ko.observable(this.text); //variant 1
+            this.observe('text'); //variant 2 приймає рядок(для 1 змінної), масив рядків (для багатьох змінних)
+
+            return this; //обов'язково, бо йде чейнінг методів
         },
 
         initialize() {
             this._super();
+
+            setTimeout(() => {
+                this.isActive(false);
+                this.text('update text');
+                this.items.push('item');
+            }, 2000)
         },
 
         toggleState() {
