67c7c2fd30c44dd2b4c9d6c917eb14a3da1af62d
qwimik <qwimik@gmail.com>
2024-01-18 10:45:19 +0200

    clear repository for start Roadmap 18.01.24

--- app/design/frontend/Perspective/rozhnenko/Magento_Customer/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    config: {
-        mixins: {
-            'mage/validation': {
-                'Magento_Customer/js/validation-mixin': true
-            }
-        }
-    }
-}
--- app/design/frontend/Perspective/rozhnenko/Magento_Theme/web/js/view/child.js
+++
@@ -1,9 +0,0 @@
-define(['uiElement', 'uiEvents'], (uiElement, uiEvents) => {
-    return uiElement.extend({
-        initialize() {
-            this._super();
-
-            uiEvents.trigger('childRender', this);
-        }
-    })
-})
