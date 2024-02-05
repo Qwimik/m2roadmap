2db29d80dc0ade5fe2f51f0ff4e9d41a6acd3d7b
qwimik <qwimik@gmail.com>
2024-02-05 14:44:05 +0200

    save changes/ uiEvents

--- app/design/frontend/Perspective/m2/Perspective_Module/web/js/view/uiComponent-parent.js
+++ app/design/frontend/Perspective/m2/Perspective_Module/web/js/view/uiComponent-parent.js
@@ -1,18 +1,72 @@
-// uiCollection, uiLayout
-define(['uiCollection', 'uiLayout'], (uiCollection, uiLayout) => {
+// render child element from file with uiEvents
+define(['uiCollection', 'uiLayout', 'uiEvents'], (uiCollection, uiLayout, uiEvents) => {
     return uiCollection.extend({
         defaults: {
             template: 'Perspective_Module/uiComponent-parent'
         },
 
-        // створення нового child component всередені parent component
+        initialize() {
+            this._super();
+
+            uiEvents.on('childRender', this.onChildRender.bind(this));
+        },
+
         addChild() {
             uiLayout([{
                 parent: this.name,
                 name: `child-${new Date().getTime()}`,
-                component: 'uiElement',
+                component: 'Perspective_Module/js/view/uiComponentChild',
                 template: 'Perspective_Module/uiComponent-child'
-            }])
+            }]);
+        },
+        onChildRender(child) {
+            console.log(child);
         }
     });
 });
+
+// uiCollection, uiLayout
+// define(['uiCollection', 'uiLayout'], (uiCollection, uiLayout) => {
+//     return uiCollection.extend({
+//         defaults: {
+//             template: 'Perspective_Module/uiComponent-parent'
+//         },
+//         timer: null,
+
+        // створення нового child component всередені parent component
+        // приклад, який показує, що this.getChild(name); відбувається синхронно, до uiLayout(який асинхронний),
+        // через що child завжди буде undefined
+
+        // addChild() {
+        //     const name = `child-${new Date().getTime()}`;
+        //     uiLayout([{
+        //         parent: this.name,
+        //         name,
+        //         component: 'uiElement',
+        //         template: 'Perspective_Module/uiComponent-child'
+        //     }]);
+        //     const child = this.getChild(name);
+        //     console.log(child);
+        // }
+
+        // інший варіант без uiEvents
+//         addChild() {
+//             const name = `child-${new Date().getTime()}`;
+//             uiLayout([{
+//                 parent: this.name,
+//                 name,
+//                 component: 'uiElement',
+//                 template: 'Perspective_Module/uiComponent-child'
+//             }]);
+//
+//             if (this.timer) clearInterval(this.timer);
+//
+//             this.timer = setInterval(() => {
+//                 const child = this.getChild(name);
+//                 console.log(child);
+//
+//                 if (child) clearInterval(this.timer);
+//             }, 100);
+//         }
+//     });
+// });
