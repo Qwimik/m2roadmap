9144d805c033da4fcfefa74ec419fb83478e1862
qwimik <qwimik@gmail.com>
2024-02-07 15:50:19 +0200

    decomposition components(view,action,model)/state management

---
+++ app/design/frontend/Perspective/m2/Perspective_Module/web/js/action/uiComponentDecomposition.js
@@ -0,0 +1,7 @@
+// action зазвичай 1 функція з 1 задачею
+// """для відправки данниї"""
+define([], () => {
+    return code => {
+        console.log('console.log in action. code: ', code)
+    }
+});
